6a7ef6d977e7e060e1cec5e22102d0ac2e3886ca
lmy <lmengyy@126.com>
2017-09-14 23:37:44 +0800

    lmy

--- base1/test.py
+++ base1/test.py
@@ -82,3 +82,56 @@ var = 5
 print(var)
 scopetest()
 print(var)  #5 6 6 5
+
+#当前版本所有关键字
+import keyword
+print(keyword.kwlist)
+
+#数据类型<3>
+#数值运算
+print(5 + 8)
+print(4.3 - 6)
+print(3 * 8)
+print(3 / 7)
+print(17 % 3)
+print(2 ** 5)
+
+#字符串
+s = 'Yes,he doesn\'t'
+print(s,"\n", type(s), "\n", len(s))
+
+#防止转义
+print('C:\some\name')
+print(r'C:\some\name')
+
+#字符串的相加 以及 chengfang
+print('str'+'ing'," \n", 'my'*3)
+
+#python的切片：与C字符串不同的是，Python字符串不能被改变。向一个索引位置赋值，比如word[0] = 'm'会导致错误。
+
+word = 'ilovepython'
+print(word[ 1 : 5])
+print(word[ 5 : ])
+print(word[ -10 : -6 ])
+
+#列表
+a = ['him', 25, 100, 'her']
+print(a)
+
+#相加
+b = [1, 2, 3]
+c = [8, 9]
+print(b + c)
+
+#支持切片操作
+s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+s[0] = 77
+print(s)
+s[2 : 5] = [45, 89]
+print(s)
+
+
+
+#元组：
+a = (1991, 2014, 'physics', 'math')
+print(a, type(a), len(a))
