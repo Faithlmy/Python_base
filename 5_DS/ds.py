8882860871e820c86c0e3653f08abf837a804d25
F1333615 <faith.my.liu@mail.foxconn.com>
2017-09-20 20:42:53 +0800

    lmy

--- 5_DS/ds.py
+++ 5_DS/ds.py
@@ -32,7 +32,7 @@ f = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 # print(s)
 # s.insert(1, 200)
 # print(s)
-
+"""=================================================================================="""
 
 """Queues"""
 # from collections import deque
@@ -79,12 +79,12 @@ f = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 # cc = [str(round(pi, i)) for i in range(0,3)]
 # print(cc)
 
-
+"""==========================================================================="""
 """矩阵的表示1"""
 matrix = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
 ]
 # m = [[row[i] for row in matrix] for i in range(4)]
 # print(m)
@@ -98,7 +98,7 @@ matrix = [
 """矩阵表示3"""
 #print(list(zip(*matrix)))
 
-
+"""==================================================================================="""
 """del用法"""
 # a = [-1, 1, 66.25, 333, 333, 1234.5]
 # del a[0]
@@ -106,6 +106,7 @@ matrix = [
 # #del a[:]
 # print(a)
 
+"""================================================================================================"""
 """元组"""
 # tup = 23, 34, 45, 'faith'
 # print(tup)
@@ -134,7 +135,76 @@ matrix = [
 #
 # nt = {x for x in b if x not in 'abc'}
 # print(nt)
-
+"""======================================================================================================"""
 """字典"""
+"""first"""
+#tel = {'c': 3, 'a': 1, 'd': 4, 'b': 2, }
+# print(tel)
+# print(tel.keys())
+# print(tel.pop('a'))#移除a，并将1返回
+# print(sorted(tel.keys()))
+# print(sorted(tel.values()))
 
+"""second"""
+# tel['f'] = 5#添加f
+# print(tel)
+# del tel['b']#移除b
+# print(tel)
 
+"""dictionaries的4种表示，自己感觉第3种有点麻烦"""
+#  1
+#print({x: x ** 2 for x in range(5)})
+#x下面这两种写法会报语法错误
+#print([x, x ** 2 for x in range(5)])
+#print([x: x ** 2 for x in range(5)])
+
+#  2
+# dt = dict(z = 26, x = 24, w = 23, y = 25)#字典的顺序不同
+# print(dt)
+
+# 3
+# d3 = dict([('m', 3), ('n', 4), ('k', 7)])
+# print(d3)
+
+#  4  最开始就用了
+
+"""==================================================================================================="""
+"""looping 技巧"""
+# kdict = tel = {'c': 3, 'a': 1, 'd': 4, 'b': 2, }
+# for x, y in kdict.items():
+#     print(x, y)
+#
+# for i, j in enumerate(['ta', 'tb', 'tc']):
+#     print(i, j)
+
+"""zip的表演"""
+# questions = ['name', 'quest', 'favorite color']
+# answers = ['lancelot', 'the holy grail', 'blue']
+# for q, a in zip(questions, answers):
+#     print('what is your {0}?  It is {1}.'.format(q, a))
+
+"""reversed"""
+# for i in reversed(range(1, 10, 2)):
+#     print(i)
+
+"""sorted"""
+# a = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+# for i in sorted(a):
+#     print(i)
+
+"""looping create new list"""
+# import math
+# rd = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
+# fd = []
+# for v in rd:
+#     if not  math.isnan(v):
+#         fd.append(v)
+# print(fd)
+
+"""=====  5.7  more on conditions  ======"""
+""" 1、 or 输出or串从左到右第一个非空字符"""
+s1, s2, s3, s4 = '', 'a', 'k', 's',
+n = s3 or s2 or s4
+m = s1 or s2 or s4
+print(n)
+print(m)
