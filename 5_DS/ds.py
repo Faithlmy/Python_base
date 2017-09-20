7943954d228c29e62414c5f461bf81d5bd4a3ab0
F1333615 <faith.my.liu@mail.foxconn.com>
2017-09-20 08:58:59 +0800

    lmy

--- 5_DS/ds.py
+++ 5_DS/ds.py
@@ -1,66 +1,83 @@
+"""list.append"""
 
-"""stacks"""
-s = [3, 4, 5]
-s.append(6)
-print(s)
-s.append(7)
-print(s)
+#list操作（11个）
+f = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+# print(f.count('banana'))
+# #f.append('pp')
+# f[len(f) : ] = ['hello']#和f.append()的效果一样
+# #f.insert(3, 'banana')
+# #f.extend('meng')
+# f[len(f):] = 'yao '#和f.extend('yao')一样
+# f.remove('y')
+#f.pop(3)
+#f.clear()
+#del f[:]
+#f.index('meng', 4)
+#print(f)
 
-print(s.pop())#删除最新的一个元素，或者指定一个位置上的元素
-print(s.pop(2))
-print(s)
-s.insert(3, 9)#添加指定位置的元素
-print(s)
-s.insert(6, 100)
-print(s)
-s.insert(1, 200)
-print(s)
+
+"""stacks."""
+# s = [3, 4, 5]
+# s.append(6)
+# print(s)
+# s.append(7)
+# print(s)
+#
+# print(s.pop())#删除最新的一个元素，或者指定一个位置上的元素
+# print(s.pop(2))
+# print(s)
+# s.insert(3, 9)#添加指定位置的元素
+# print(s)
+# s.insert(6, 100)
+# print(s)
+# s.insert(1, 200)
+# print(s)
 
 
 """Queues"""
-from collections import deque
-q = deque(["A", "B", "C"])
-q.append("D")
-print(q)
-print(q.popleft())
+# from collections import deque
+# q = deque(["A", "B", "C"])
+# q.append("D")
+# print(q)
+# print(q.popleft())
 
 
 """求平方或者立方"""
-sq = []
-for x in range(10):
-    sq.append(x ** 3)
-print(sq)
-
-sq = list(map(lambda x: x**3, range(10)))
-print(sq)
-
-sq = [x**2 for x in range(10)]
-print(sq)
+# sq = []
+# for x in range(10):
+#     sq.append(x ** 3)
+# print(sq)
+#
+# sq = list(map(lambda x: x**3, range(10)))
+# print(sq)
+#
+# sq = [x**2 for x in range(10)]
+# print(sq)
 
 """组合"""
-c = []
-for i in range(4):
-    for j in range(2):
-        if i != j:
-            c.append((i, j))
-print(c)
+# c = []
+# for i in range(4):
+#     for j in range(2):
+#         if i != j:
+#             c.append((i, j))
+# print(c)
 
 
 """求平方, [里面直接计算]"""
-s = [-6, -5, -2, 0, 1, 3]
-print([x ** 2 for x in s])
-
-print('*'*9)
-print([x ** 2  for x in range(5)])
+# s = [-6, -5, -2, 0, 1, 3]
+# print([x ** 2 for x in s])
+#
+# print('*'*9)
+# print([x ** 2  for x in range(5)])
 
 """拆开"""
-vec = [[1,2,3], [4,5,6], [7,8,9]]
-print([num for elem in vec for num in elem])
+# vec = [[1,2,3], [4,5,6], [7,8,9]]
+# print([num for elem in vec for num in elem])
 
 """pi的演示"""
-from math import pi
-cc = [str(round(pi, i)) for i in range(0,3)]
-print(cc)
+# from math import pi
+# cc = [str(round(pi, i)) for i in range(0,3)]
+# print(cc)
 
 
 """矩阵的表示1"""
@@ -69,22 +86,55 @@ matrix = [
     [5,6,7,8],
     [9,10,11,12],
 ]
-m = [[row[i] for row in matrix] for i in range(4)]
-print(m)
+# m = [[row[i] for row in matrix] for i in range(4)]
+# print(m)
 
 """矩阵的表示2"""
-t = []
-for i in range(4):
-    t.append([row[i] for row in matrix])
-print(t)
+# t = []
+# for i in range(4):
+#     t.append([row[i] for row in matrix])
+# print(t)
 
 """矩阵表示3"""
-print(list(zip(*matrix)))
+#print(list(zip(*matrix)))
 
 
-"""del"""
-a = [-1, 1, 66.25, 333, 333, 1234.5]
-del a[0]
-#del a[2:4]
-#del a[:]
-print(a)
+"""del用法"""
+# a = [-1, 1, 66.25, 333, 333, 1234.5]
+# del a[0]
+# #del a[2:4]
+# #del a[:]
+# print(a)
+
+"""元组"""
+# tup = 23, 34, 45, 'faith'
+# print(tup)
+# u = tup, (12, 13, 14)
+# print(u)
+
+"""借用上面的元组，测试in的用法"""
+# print(23 in tup)#T
+# print((23, 34, 45) in tup)#F
+# print((23, 34, 45, 'faith') in u)#T
+# print((23, 34, 45, 'faith') in tup)#F
+# print(tup in u)#T
+# print(100 in tup)#F
+
+"""set配合 逻辑运算符的用法"""
+#set会自动过滤字符串里面重复的字符，并无序输出
+#没有a+b
+# a = set('abbgkk')
+# b = set('aabccd')
+# print(a)
+# print(b)
+# print(a - b)
+# print(a | b)#相当于a+b
+# print(a & b)
+# print(a ^ b)
+#
+# nt = {x for x in b if x not in 'abc'}
+# print(nt)
+
+"""字典"""
+
+
